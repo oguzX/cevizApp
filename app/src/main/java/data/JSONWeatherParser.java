a64294fbd2be122515444a629d561420f4f08d11
Hasan Yesil <hasannyesil97@gmail.com>
2019-04-20 00:25:22 +0300

    Open Weather API implementasyonu

---
+++ app/src/main/java/data/JSONWeatherParser.java
@@ -0,0 +1,59 @@
+package data;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import Utils.Utils;
+import model.Place;
+import model.Weather;
+
+public class JSONWeatherParser {
+
+    public static Weather getWeather(String data) {
+
+        Weather weather = new Weather();
+
+        //create JsonObject from data
+        try {
+            JSONObject jsonObject = new JSONObject(data);
+
+            Place place = new Place();
+
+            JSONObject coorObj = Utils.getObject("coord",jsonObject);
+            place.setLat(Utils.getFloat("lat",coorObj));
+            place.setLon(Utils.getFloat("lon",coorObj));
+
+            //Get the sys obj
+            JSONObject sysObj = Utils.getObject("sys",jsonObject);
+            place.setCountry(Utils.getString("country",sysObj));
+            place.setLastupdate(Utils.getInt("dt",jsonObject));
+            place.setSunrise(Utils.getInt("sunrise",sysObj));
+            place.setSunset(Utils.getInt("sunset",sysObj));
+            place.setCity(Utils.getString("name",jsonObject));
+            weather.place = place;
+
+            //Get the weather info
+            JSONArray jsonArray = jsonObject.getJSONArray("weather");
+            JSONObject jsonWeather = jsonArray.getJSONObject(0);
+            weather.currentCondition.setWeatherId(Utils.getInt("id",jsonWeather));
+            weather.currentCondition.setDescription(Utils.getString("description",jsonWeather));
+            weather.currentCondition.setCondition(Utils.getString("main",jsonWeather));
+            weather.currentCondition.setIcon(Utils.getString("icon",jsonWeather));
+
+            JSONObject windObj = Utils.getObject("wind",jsonObject);
+            weather.wind.setSpeed(Utils.getFloat("speed",windObj));
+            weather.wind.setDeg(Utils.getFloat("deg",windObj));
+
+            JSONObject cloudsObj = Utils.getObject("clouds",jsonObject);
+            weather.clouds.setPrecipitation(Utils.getInt("all",cloudsObj));
+
+            return weather;
+
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}

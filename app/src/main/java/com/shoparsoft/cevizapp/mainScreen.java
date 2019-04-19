5befea31c33e3612793ca0713210d025f743a03c
Hasan Yesil <hasannyesil97@gmail.com>
2019-04-19 21:52:39 +0300

    Json nesnesinden veri cekecek fonksiyonlar ve gradle guncellemesi

--- app/src/main/java/com/shoparsoft/cevizapp/mainScreen.java
+++ app/src/main/java/com/shoparsoft/cevizapp/mainScreen.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 
 public class mainScreen extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
     }
+
 }
